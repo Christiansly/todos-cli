0d8b812417b89b14256de9d587d24d8c2c630a33
Christiansly <abiode23@gmail.com>
2022-05-07 02:01:13 +0100

    first commit

---
+++ commands/list.js
@@ -0,0 +1,23 @@
+import confInstance from "conf"
+const conf = new confInstance();
+import chalk from "chalk";
+export default function list() {
+  const todoList = conf.get("todo-list");
+  if (todoList && todoList.length) {
+    console.log(
+      chalk.blue.bold(
+        "Tasks in green are done. Tasks in yellow are still not done."
+      )
+    );
+    todoList.forEach((task, index) => {
+      if (task.done) {
+        console.log(chalk.greenBright(`${index}. ${task.text}`));
+      } else {
+        console.log(chalk.yellowBright(`${index}. ${task.text}`));
+      }
+    });
+  } else {
+    console.log(chalk.red.bold("You don't have any tasks yet."));
+  }
+}
+
